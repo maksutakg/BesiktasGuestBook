d25bbe08ac3251c4ed599e843efa533f7bd4c33b
maksut <maksutakgun4@gmail.com>
2026-02-02 22:47:55 +0300

    fix

--- Domain/Common/EntityBase.cs
+++ Domain/Common/EntityBase.cs
@@ -9,7 +9,7 @@ namespace Domain.Common
    public class EntityBase
     {
         public int Id { get; set; }
-        public DateTime DateTime { get; set; } = DateTime.Now;
+        public DateTime DateTime { get; set; } = DateTime.UtcNow;
         public bool IsDeleted { get; set; } = false;
         public bool IsActive { get; set; } = true;
         public string Role { get; set; } = "User";
